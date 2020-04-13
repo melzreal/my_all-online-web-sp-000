5f3b2ff476fc81c9fff25219ca577a0e40762f22
Melissa Real <melissa.real@hotmail.com>
2020-04-13 11:59:46 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,4 +7,10 @@ def my_all?(collection)
      vals << yield(collection[i])
      i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
    end
+
+   if vals.include(false)
+     false 
+   else 
+     true 
+   end 
 end
