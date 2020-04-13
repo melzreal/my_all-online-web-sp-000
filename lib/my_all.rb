53825772aa7f0182d44a890bb4364e8588b0abee
Melissa Real <melissa.real@hotmail.com>
2020-04-13 12:00:26 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,9 +8,9 @@ def my_all?(collection)
      i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
    end
 
-   if vals.include(false)
+   if vals.include?(false)
      false
    else
      true
-   end 
+   end
 end
